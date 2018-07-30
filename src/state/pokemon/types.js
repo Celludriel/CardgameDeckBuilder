a96b0ad4c5a4c7ba77593fa32562917ff9e96e90
Kenny De Smedt <de_smedt_kenny@telenet.be>
2018-07-30 18:46:15 +0200

    some cleanup

--- src/state/pokemon/types.js
+++ src/state/pokemon/types.js
@@ -1,9 +1,7 @@
 const START_LOAD_DATABASE = "app/pokemon/START_LOAD_DATABASE";
-const LOAD_DATABASE = "app/pokemon/LOAD_DATABASE";
 const END_LOAD_DATABASE = "app/pokemon/END_LOAD_DATABASE";
 
 export default {
     START_LOAD_DATABASE,
-    LOAD_DATABASE,
     END_LOAD_DATABASE
 };
